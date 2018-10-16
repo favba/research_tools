aeabc88f23ec4518adf922ee91236c086cecd73e
Felipe Alves <favbalves@gmail.com>
2018-10-16 14:54:30 -0400

    stress_state script

---
+++ stress_state.jl
@@ -0,0 +1,17 @@
+#!/usr/bin/env julia
+
+using LinearAlgebra, ReadGlobal, FluidTensors
+
+function stress(S,o)
+    Threads.@threads for i in LinearIndices(S)
+        @inbounds o[i] = stress_state(S[i])
+    end
+end
+
+function run()
+    inp = (SymTenArray=>(ARGS[1:6]...),)
+    out = (Vector{Float64}=>(ARGS[7],))
+    doinchunks(stress,input=inp,output=out)
+end
+
+run()
