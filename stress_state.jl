ef4d31bbcf47a7705985a16e20d139ff9e5f06b1
Felipe Alves <favbalves@gmail.com>
2018-10-16 15:00:49 -0400

    fix out

--- stress_state.jl
+++ stress_state.jl
@@ -10,7 +10,7 @@ end
 
 function run()
     inp = (SymTenArray=>(ARGS[1:6]...,),)
-    out = (Vector{Float64}=>(ARGS[7],))
+    out = (Vector{Float64}=>(ARGS[7],),)
     doinchunks(stress,input=inp,output=out)
 end
 
