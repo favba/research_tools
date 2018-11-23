accabc8b5c8012551943e942614c4ac929fcf61a
Felipe Alves <favbalves@gmail.com>
2018-11-23 18:39:13 -0500

    Make energy redistribution tensor consistent with before.

--- makeEnergyDir.jl
+++ makeEnergyDir.jl
@@ -3,15 +3,15 @@
 # Script to compute the energy redistribution tensor
 #
 # Usage: (Note that the 33 elements should not be inputed)
-#                  |----------------- input --------------------------||---------- output -------------| 
-# makeEnergyDir.jl S11 S12 S13 S22 S23 W12 W13 W23 T11 T12 T13 T22 T23 dir pr
+#                  |----------------- input --------------------------||- output -| 
+# makeEnergyDir.jl S11 S12 S13 S22 S23 W12 W13 W23 T11 T12 T13 T22 T23   dir pr
 using FluidTensors, GlobalFileHelper, LinearAlgebra
 
 function calc(S,W,T,dir,pr)
     Threads.@threads for i in eachindex(pr)
         @inbounds begin
-            R = SymTen(T[i]⋅(S[i]+W[i]) + (S[i]-W[i])⋅T[i]) 
-            pr[i] = tr(R)/2
+            R = SymTen(T[i]⋅(S[i]+W[i]) + (S[i]-W[i])⋅T[i])/2
+            pr[i] = tr(R)
             dir[i] = traceless(R)
         end
     end
@@ -29,14 +29,5 @@ function run()
     out = (SymTenArray{T} => (ARGS[14] .* ("11","12","13","22","23","33")), Vector{T} =>(ARGS[15],))
     doinchunks(calc,input=inp,output=out)
 end
-
-#S = SymTrTenArray(ARGS[1:5]...)
-#W = AntiSymTenArray(ARGS[6:8]...)
-#T = SymTrTenArray(ARGS[9:13]...)
-
-#@. T = traceless(SymTen(T⋅(S+W) + (S-W)⋅T))
-
-#write((ARGS[14:18]...,),T)
-#write(string(SubString(ARGS[18],1,lastindex(ARGS[18])-2),"33"),(-).(T.xx .+ T.yy))
 
 run()
