8e89dd15d431f2b520c52debe1c1ece704ec75b4
Felipe Alves <favbalves@gmail.com>
2019-03-09 17:18:05 -0500

    Plot more than one curve

--- pstats.jl
+++ pstats.jl
@@ -2,9 +2,13 @@
 
 using ReadGlobal, UnicodePlots
 
-function main()
+function main(ARGS::Vector{String})
     d = readcsv("Stats.txt")
-    show(lineplot(d.time,getproperty(d,Symbol(ARGS[1])),xlabel="time",ylabel=ARGS[1]))
+    plt = lineplot(d.time,getproperty(d,Symbol(ARGS[1])),xlabel="time",name=ARGS[1])
+    for i=2:length(ARGS)
+        lineplot!(plt,d.time,getproperty(d,Symbol(ARGS[i])),name=ARGS[i])
+    end
+    display(plt)
 end
 
-main()
+main(ARGS)
