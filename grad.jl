bbf8bf3d25f7e2675a7ebef054d68394b858b595
Felipe Alves <favbalves@gmail.com>
2019-04-10 12:10:41 -0400

    optional output posfix

--- grad.jl
+++ grad.jl
@@ -3,8 +3,8 @@
 # Script to compute the gradient of fields
 #
 #             
-# grad.jl input-scalar-file output-name
-# grad.jl input-vector-file1 input-vector-file3 input-vector-file3 output_symmetric-name output-antisymmetric-name
+# grad.jl input-scalar-file output-name [output-posfix]
+# grad.jl input-vector-file1 input-vector-file3 input-vector-file3 output_symmetric-name output-antisymmetric-name [output-posfix]
 
 using FluidFields, GlobalFileHelper, FluidTensors, LinearAlgebra
 
@@ -53,29 +53,45 @@ function grad_vec(vec::VectorField)
     return grad_vec!(sym,asym,vec)
 end
 
-function grad(s::AbstractString,v::AbstractString)
+function grad(s::AbstractString,v::AbstractString,p::AbstractString)
     scalar = ScalarField(s)
     vec = grad_scalar(scalar)
-    write(string(v,"1"),vec.c.x.r)
-    write(string(v,"2"),vec.c.y.r)
-    write(string(v,"3"),vec.c.z.r)
+    write(string(v,"1",p),vec.c.x.r)
+    write(string(v,"2",p),vec.c.y.r)
+    write(string(v,"3",p),vec.c.z.r)
     return nothing
 end
 
-function grad(v1::AbstractString,v2::AbstractString,v3::AbstractString,S::AbstractString,W::AbstractString)
+function grad(v1::AbstractString,v2::AbstractString,v3::AbstractString,S::AbstractString,W::AbstractString,p::AbstractString)
     vec = VectorField(v1,v2,v3)
     sym,asym = grad_vec(vec)
-    write(string(S,"11"),sym.c.xx.r)
-    write(string(S,"12"),sym.c.xy.r)
-    write(string(S,"13"),sym.c.xz.r)
-    write(string(S,"22"),sym.c.yy.r)
-    write(string(S,"23"),sym.c.yz.r)
+    write(string(S,"11",p),sym.c.xx.r)
+    write(string(S,"12",p),sym.c.xy.r)
+    write(string(S,"13",p),sym.c.xz.r)
+    write(string(S,"22",p),sym.c.yy.r)
+    write(string(S,"23",p),sym.c.yz.r)
     
-    write(string(W,"12"),asym.c.z.r)
-    write(string(W,"13"),-asym.c.y.r)
-    write(string(W,"23"),asym.c.x.r)
+    write(string(W,"12",p),asym.c.z.r)
+    write(string(W,"13",p),-asym.c.y.r)
+    write(string(W,"23",p),asym.c.x.r)
 
     return nothing
 end
 
-grad(ARGS...)
+#grad(ARGS...)
+function main(args)
+    if length(args) == 2
+        grad(args[1],args[2],"")
+    elseif length(args) == 3
+        grad(args[1],args[2],args[3])
+    elseif length(args) == 5
+        grad(args[1],args[2],args[3],args[4],args[5],"")
+    elseif length(args) == 6
+        grad(args[1],args[2],args[3],args[4],args[5],args[6])
+    else
+        error("Wrong number of arguments")
+    end
+    return 0
+end
+
+main(ARGS)
